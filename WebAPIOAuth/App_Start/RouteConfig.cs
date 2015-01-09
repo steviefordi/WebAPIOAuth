e4caa3f597a300fb94950166e5a63c5efe2a8061
Stephen Ford <sford@scottlogic.com>
2015-01-09 16:52:00 +0000

    Step 2 Completed

--- WebAPIOAuth/App_Start/RouteConfig.cs
+++ WebAPIOAuth/App_Start/RouteConfig.cs
@@ -12,12 +12,12 @@ namespace WebAPIOAuth
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{*path}",
+                defaults: new { controller = "Home", action = "Index" }
             );
+            
         }
     }
 }
