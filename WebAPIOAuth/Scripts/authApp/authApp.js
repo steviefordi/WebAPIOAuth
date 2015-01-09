6e87bd61dfefc5f60c2f33a2713d7c4815a97ce3
Stephen Ford <sford@scottlogic.com>
2015-01-09 16:55:29 +0000

    Step 3 Completed

--- WebAPIOAuth/Scripts/authApp/authApp.js
+++ WebAPIOAuth/Scripts/authApp/authApp.js
@@ -1,1 +1,28 @@
 ﻿var app = angular.module('authApp', ['ngRoute']);
+
+app.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider.
+        when('/', {
+            templateUrl: 'Scripts/authApp/home.html'
+        }).
+        when('/values', {
+            templateUrl: 'Scripts/authApp/values.html',
+            controller: 'valuesController'
+        });
+});
+
+app.controller('valuesController', ['$scope', '$http', function ($scope, $http) {
+    $scope.errorGettingValues = false;
+    $scope.errorCode = null;
+
+    $http({
+        url: '/api/Values',
+        method: "GET"
+    }).success(function (data) {
+        $scope.values = data;
+    }).error(function (data, status) {
+        $scope.errorCode = status;
+        $scope.errorGettingValues = true;
+    });
+}]);
